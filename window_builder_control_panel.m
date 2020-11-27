212372e6155a601d20151fb1d3ea0dac1adcbf75
SamZebrado <samzebrado@foxmail.com>
2020-11-27 16:05:08 +0800

    save a prototype funtion for building windows; some misuse of persistent fixed

--- window_builder_control_panel.m
+++ window_builder_control_panel.m
@@ -1,3 +1,9 @@
 function hndl = window_builder_control_panel()
-
+% create window if called for the first time, return existing window if has
+% been called before.
+persistent figure_hndl
+if ~isempty(figure_hndl)
+    figure_hndl = figure;
+end
+hndl = figure_hndl;
 end
